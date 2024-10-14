a2caf31423bbf5621908e6070d86dc0ca16d2920
Duong Hoang Viet <vietdhhe141132@fpt.edu.vn>
2024-10-14 23:25:09 +0700

    DHV11: commit source base

---
+++ TheCoffeeHouse-Server/TheCoffeeHouse-Server/TheCoffeeHouse_ServerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheCoffeeHouse_ServerApp.swift
+//  TheCoffeeHouse-Server
+//
+//  Created by Việt Dương Hoàng on 14/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TheCoffeeHouse_ServerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
